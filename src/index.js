479bc2205e77be7a4456fe2070a28ca803f2da29
Jesse Hall <jessehall3@gmail.com>
2021-12-19 13:26:45 -0600

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,86 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+class Square extends React.Component {
+  constructor() {
+    super()
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  handleClick() {
+    alert(this.props.value)
+  }
+
+  render() {
+    return (
+      <button 
+        className="square"
+        onClick={this.handleClick}
+      >
+        {this.props.value}
+      </button>
+    );
+  }
+}
+
+class Board extends React.Component {
+  static COLUMNS = 3
+  static ROWS = 3
+
+  renderSquare(i) {
+    return <Square value={i} key={`square-${i}`}/>;
+  }
+
+  renderRow(row) {
+    const squares = Array(Board.COLUMNS).fill(null).map((_, col) => {
+      return this.renderSquare(row + col)
+    })
+
+    return (
+      <div className="board-row" key={`row-${row}`}>
+        {squares}
+      </div>
+    )
+  }
+
+  renderRows() {
+    return Array(Board.ROWS).fill(null).map((_, row) => {
+      return this.renderRow(Board.ROWS * row)
+    }) 
+  }
+
+  render() {
+    const status = 'Next player: X';
+
+    return (
+      <div>
+        <div className="status">{status}</div>
+        {this.renderRows()}
+      </div>
+    );
+  }
+}
+
+class Game extends React.Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+// ========================================
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Game />,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
